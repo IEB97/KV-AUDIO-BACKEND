b0c0d0fb1686ea943b2ea1b9622266ec9240199e
IEB97 <isharaeranda1997@gmail.com>
2025-02-23 20:05:04 +0530

    body parser and nodemon install 🤷‍♂️

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import  express  from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 let app = express();
+app.use(bodyParser.json());
 
-app .get('/', 
-    (req,res) => {
-    console.log("This is Get request")
-    res.json({message: "Hello World"})
-}  
-);
+app.get('/', 
+    (req, res) => {
+    console.log("This is a Get request");
+    res.json({
+        "message": "Good Morning " + req.body.name,
+    });
+});
 
-app.post('/',
-    (req,res) => {
-    console.log("This is post request")
-    res.json({message: "That is a post request",status: "success"})
-}
-);
+app.post('/', (req, res) => {
+    console.log("This is post request");
+    res.json({
+        message: "That is a post request",
+        status: "success"
+    });
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-}
-);
+    console.log('Server is running on port 3000');
 
+});

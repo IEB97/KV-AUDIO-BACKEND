b65e7eb8af7bc50fa09ce2f9e1978529e9f33b1d
IEB97 <isharaeranda1997@gmail.com>
2025-02-23 22:08:24 +0530

    data base connection established and data base data added👌👍😁

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 let app = express();
 app.use(bodyParser.json());
+
+let mongoURL = "mongodb+srv://IEB97:123@cluster0.p3sm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoURL);
+
+let connection = mongoose.connection;
+    connection.once ("open", () => {
+        console.log("MongoDB connection established successfully");
+    } 
+);       
 
 app.get('/', 
     (req, res) => {
@@ -12,13 +22,37 @@ app.get('/',
     });
 });
 
-app.post('/', (req, res) => {
-    console.log("This is post request");
-    res.json({
-        message: "That is a post request",
-        status: "success"
+let studentSchema = new mongoose.Schema(
+    {
+    name: String,
+    age: Number,
+    hight: Number,
+}
+);
+
+let Student = mongoose.model('Students', studentSchema);
+
+app.post('/', 
+    (req, res) => {
+        let newStudent = req.body;
+        let student = new Student(newStudent);
+            student.save().then( 
+                 () => {
+                  console.log("Student Save Successfully");  
+                  res.json({
+                    "Message": "Student Save Successfully"
+                })
+        }
+        ).catch(
+            () => {
+                res.json({
+                    "Message": "Student could not be saved"
+                })
+            }
+        )
     });
-});
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

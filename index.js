3f4691403ecd1b8a8b6a6316678ae7a5bbc1e267
IEB97 <isharaeranda1997@gmail.com>
2025-02-23 18:57:30 +0530

    postmen requests👀

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ import  express  from 'express';
 
 let app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app .get('/', 
+    (req,res) => {
+    console.log("This is Get request")
+    res.json({message: "Hello World"})
+}  
+);
 
+app.post('/',
+    (req,res) => {
+    console.log("This is post request")
+    res.json({message: "That is a post request",status: "success"})
+}
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+}
+);
 

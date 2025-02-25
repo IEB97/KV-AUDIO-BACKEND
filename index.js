ba260129f85030f183948e64ffe0bfe0d9804200
IEB97 <isharaeranda1997@gmail.com>
2025-02-25 23:07:11 +0530

    const

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import studentRouter from './routes/studentRoutes.js';
 
-let app = express();
+const app = express();
 app.use(bodyParser.json());
 
 let mongoURL = "mongodb+srv://IEB97:123@cluster0.p3sm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(mongoURL);
 
-let connection = mongoose.connection;
+const connection = mongoose.connection;
     connection.once ("open", () => {
         console.log("MongoDB connection established successfully");
     } 

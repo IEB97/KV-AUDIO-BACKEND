c16b107c3a5fa55d2fff163868e4c653c1ab14de
IEB97 <isharaeranda1997@gmail.com>
2025-04-20 23:12:17 +0530

    Day 5 Complete😁 (Product authorized manage)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 export function registerUser(req, res) {
 
@@ -28,7 +31,13 @@ export function loginUser(req, res) {
                 const isPasswordCorrect = bcrypt.compareSync(data.password, user.password);
 
                 if (isPasswordCorrect) {
-                    res.json({message: "Login successful"});
+                     const token = jwt.sign({
+                        firtName : user.firstName,
+                        lastName : user.lastName,
+                        email : user.email,
+                        role : user.role,
+                     },"kv-secret-89!") 
+                    res.json({message: "Login successful" , token: token});
                 } else {
                     res.status(401).json({error: "Login Failed"});
                 }
--- index.js
+++ index.js
@@ -2,9 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userrouter from './routes/userrouter.js';
+import productRouter from './routes/productRouter.js';
+import jwt from "jsonwebtoken";
+
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    let token = req.headers('authorization');
+    if (token!=null){
+        token = token.replace("Bearer ", "");
+
+        jwt.verify(token, "kv-secret-89!",
+            (err, decoded) => {
+                if(!err){
+                    req.user = decoded;
+                }})
+
+    }
+    next();
+
+})
+
+
 
 let mongoURL = "mongodb+srv://IEB97:123@cluster0.p3sm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(mongoURL);
@@ -16,6 +37,8 @@ const connection = mongoose.connection;
 );       
 
 app.use("/api/users",userrouter)
+app.use("/api/products", productRouter)
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
